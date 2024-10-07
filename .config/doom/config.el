358b6b3880e5129f9d4fa619d2644b9d0426362d
jeteitsworth <james@undercurrentbooks.com>
2024-10-07 18:16:05 -0500

    Update doom to use org-journal instead of roam

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -40,7 +40,12 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/Nextcloud/org")
+(setq org-directory "/mnt/c/Users/james.teitsworth/Nextcloud/org")
+(add-to-list 'org-agenda-files (expand-file-name "/mnt/c/Users/james.teitsworth/Nextcloud/org/journal"))
+(setq org-journal-dir "/mnt/c/Users/james.teitsworth/Nextcloud/org/journal")
+(setq org-journal-file-type 'weekly)
+(setq org-journal-file-format "%Y%m%d.org")
+
 
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
