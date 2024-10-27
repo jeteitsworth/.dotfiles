4bff695f61bd86b31232add117407518f07fb326
jeteitsworth <james@undercurrentbooks.com>
2024-10-27 18:42:47 -0500

    Finalize tools based on new PC setup

--- bin/arch_setup.sh
+++ bin/arch_setup.sh
@@ -1,1 +1,7 @@
-sudo pacman -S fzf bat lsd ripgrep tmux the_silver_searcher stow unzip texlive-latex djvulibre mupdf hunspell hunspell-en_us zip nextcloud-client rclone
+sudo pacman -S fzf bat lsd ripgrep tmux the_silver_searcher stow unzip texlive-latex djvulibre mupdf hunspell hunspell-en_us zip nextcloud-client rclone thunderbird
+
+xmodmap -e "remove lock = Caps_Lock"
+xmodmap -e "keycode 66 = Control_L"
+xmodmap -e "add control = Control_L"
+
+
