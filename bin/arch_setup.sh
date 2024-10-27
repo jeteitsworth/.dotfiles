7ac49584ed1d2a6a90687413a93cfbc6bfbffefb
jeteitsworth <james@undercurrentbooks.com>
2024-10-27 18:25:44 -0500

    Add more required tools

--- bin/arch_setup.sh
+++ bin/arch_setup.sh
@@ -1,1 +1,1 @@
-sudo pacman -S fzf bat lsd ripgrep tmux the_silver_searcher stow
+sudo pacman -S fzf bat lsd ripgrep tmux the_silver_searcher stow unzip texlive-latex djvulibre mupdf hunspell hunspell-en_us zip nextcloud-client rclone
