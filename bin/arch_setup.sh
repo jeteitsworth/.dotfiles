35a280aae363dd4ad6055fa367017473f1e7f29e
jeteitsworth <james@undercurrentbooks.com>
2024-10-27 18:45:51 -0500

    I always forget node

--- bin/arch_setup.sh
+++ bin/arch_setup.sh
@@ -1,4 +1,4 @@
-sudo pacman -S fzf bat lsd ripgrep tmux the_silver_searcher stow unzip texlive-latex djvulibre mupdf hunspell hunspell-en_us zip nextcloud-client rclone thunderbird
+sudo pacman -S fzf bat lsd ripgrep tmux the_silver_searcher stow unzip texlive-latex djvulibre mupdf hunspell hunspell-en_us zip nextcloud-client rclone thunderbird nodejs npm
 
 xmodmap -e "remove lock = Caps_Lock"
 xmodmap -e "keycode 66 = Control_L"
