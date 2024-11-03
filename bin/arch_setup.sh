f5300da8d3e9e67adc963074991da43fb1ba4e30
jeteitsworth <james@undercurrentbooks.com>
2024-11-02 22:25:04 -0500

    Moved one-time scripts to syncthing

--- bin/arch_setup.sh
+++
@@ -1,7 +0,0 @@
-sudo pacman -S fzf bat lsd ripgrep tmux the_silver_searcher stow unzip texlive-latex djvulibre mupdf hunspell hunspell-en_us zip nextcloud-client rclone thunderbird nodejs npm
-
-#xmodmap -e "remove lock = Caps_Lock"
-#xmodmap -e "keycode 66 = Control_L"
-#xmodmap -e "add control = Control_L"
-
-
