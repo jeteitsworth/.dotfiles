721b3339c413dd6197ed1b0320858e44a70c9e9b
jeteitsworth <james@undercurrentbooks.com>
2024-11-04 22:32:57 -0600

    Doom emacs because I don't have the bandwidth to config from scratch right now

---
+++ .config/doom/packages.el
@@ -0,0 +1,13 @@
+;; -*- no-byte-compile: t; -*-
+;;; $DOOMDIR/packages.el
+
+;; To install a package with Doom you must declare them here and run 'doom sync'
+;; on the command line, then restart Emacs for the changes to take effect -- or
+;; use 'M-x doom/reload'.
+
+
+;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
+;; (package! some-package)
+(package! org-notify)
+(package! denote)
+(package! nov)
