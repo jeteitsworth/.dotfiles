b351fcb6c48174aad7a04b4914ecfe4719049376
jeteitsworth <james@undercurrentbooks.com>
2024-11-12 17:46:25 -0600

    Update stray elisp file

--- .config/doom/custom.el
+++ .config/doom/custom.el
@@ -4,7 +4,11 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(org-agenda-files
+<<<<<<< Updated upstream
    '("~/Nextcloud/org/todo.org" "/home/james/Nextcloud/org/appointments.org")))
+=======
+   '("~/Nextcloud/org/appointments.org" "/home/james/Nextcloud/org/todo.org")))
+>>>>>>> Stashed changes
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
