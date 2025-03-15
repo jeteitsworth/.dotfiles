c648010d48674bc96a2671794dfe80aa484f05a9
James E Teitsworth <james@undercurrentbooks.com>
2025-03-15 13:55:13 -0500

    Update init.el
    Remove old settings

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -36,7 +36,7 @@
        (emoji +unicode)  ; 🙂
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;;indent-guides     ; highlighted indent columns
-       ligatures         ; ligatures and symbols to make your code pretty again
+       ;;ligatures         ; ligatures and symbols to make your code pretty again
        ;;minimap           ; show a map of the code on the side
        modeline          ; snazzy, Atom-inspired modeline, plus API
        nav-flash         ; blink cursor line after big motions
@@ -69,16 +69,16 @@
        :emacs
        (dired +icons)
        electric          ; smarter, keyword-based electric-indent
-       eww               ; the internet is gross
+       ;;eww               ; the internet is gross
        ibuffer           ; interactive buffer management
        undo              ; persistent, smarter undo for your inevitable mistakes
-       vc                ; version-control and Emacs, sitting in a tree
+       ;;vc                ; version-control and Emacs, sitting in a tree
 
        :term
        ;;eshell            ; the elisp shell that works everywhere
        ;;shell             ; simple shell REPL for Emacs
        ;;term              ; basic terminal emulator for Emacs
-       vterm             ; the best terminal emulation in Emacs
+       ;;vterm             ; the best terminal emulation in Emacs
 
        :checkers
        syntax              ; tasing you for every semicolon you forget
@@ -185,7 +185,7 @@
        ;;zig               ; C, but simpler
 
        :email
-       (mu4e +org)
+       ;;(mu4e +org)
        ;;(notmuch +org)
        ;;(wanderlust +gmail)
 
@@ -193,7 +193,7 @@
        ;;calendar
        emms
        ;;everywhere        ; *leave* Emacs!? You must be joking
-       irc               ; how neckbeards socialize
+       ;;irc               ; how neckbeards socialize
        (rss +org)        ; emacs as an RSS reader
 
        :config
