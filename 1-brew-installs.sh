35ffbade574a2e9eb82dc8b1f796c822e5005fcb
James Teitsworth <james@jamesteitsworth.com>
2025-08-04 10:30:57 -0500

    Pandoc is a must

--- 1-brew-installs.sh
+++ 1-brew-installs.sh
@@ -1,5 +1,5 @@
 brew install gcc
 
-brew install zsh-autosuggestions zsh-syntax-highlighting powerlevel10k vim fzf bat ripgrep the_silver_searcher perl universal-ctags eza
+brew install zsh-autosuggestions zsh-syntax-highlighting powerlevel10k vim fzf bat ripgrep the_silver_searcher perl universal-ctags eza pandoc
 
 brew install --cask font-meslo-lg-nerd-font
